3791c60572cccb9d9d471936104e5e0424eb3fc2
Muhammad Nofil <nofil.khan037@gmail.com>
2023-09-12 01:03:04 +0500

    new

--- curriculam/src/index.jsx
+++ curriculam/src/index.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './index.css'
+import './index.css';
+import phone from './phone.png';
+import mail from './mail.png';
+import linkedin from './linkedin.svg';
+
 
 function Hi() {
     return (<div className="Body">
         <div className="First">
-        <div className="Education">
-            <div className="fff"></div>
-            <div className="eee">
-
+            <div className="white">
+                <div className="white-main">
+                    <h1>M.Naufel</h1>
+                    <p>Frontend Developer </p>
+                    <br /><br />
+                    <h3>Profile:</h3>
+                    <p className="para-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere molestias, facilis doloribus nobis mollitia aspernatur, nihil eum repudiandae numquam corrupti, sint incidunt distinctio totam ipsa quae dicta quibusdam adipisci vel!</p>
+                <br />
+                   <a href="tel:+923240539139"> +92 324-0539-139 <img src={phone} alt="img" /></a>
+                   <a href="mailto:">Nofil.khan037@gmail.com<img src={mail} alt="img" /> </a>
+                   <a href="#"> Muhammad Nofil<img src={linkedin} alt="img" /></a>
+                </div>
             </div>
-        </div>
+            <div className="Black">
+                <h1>Jeelani</h1>
+            </div>
+
         </div>
     </div>)
 }
