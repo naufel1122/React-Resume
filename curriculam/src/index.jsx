26e5f18f64119516db8be24d66bde6547229de2a
Muhammad Nofil <nofil.khan037@gmail.com>
2023-09-11 15:43:30 +0500

    nw resume

---
+++ curriculam/src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css'
+
+function Hi() {
+    return (<div className="Body">
+        <div className="First">
+        <div className="Education">
+
+        </div>
+        </div>
+    </div>)
+}
+ReactDOM.render(<Hi />, document.querySelector("#root"));
