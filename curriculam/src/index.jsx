002906e6fa7d5d200b4b88e42af680183266eb6c
Muhammad Nofil <nofil.khan037@gmail.com>
2023-09-23 18:40:19 +0500

    first side almost done

--- curriculam/src/index.jsx
+++ curriculam/src/index.jsx
@@ -12,7 +12,7 @@ function Hi() {
             <div className="white">
                 <div className="img">
 
-              <img  src={myimg} alt="" />
+                    <img src={myimg} alt="" />
                 </div>
                 <h3>EDUCATION</h3>
                 <div className="line"></div>
@@ -23,7 +23,37 @@ function Hi() {
                 <h3>REFERENCE</h3>
                 <div className="line"></div>
                 <p className="merit" >On Merit</p>
+
+                <div className="phone">
+                    <div className="empty"></div>
+                    <div className="contact">
+                        <p>Phone</p>
+                    </div>
+                </div>
+                    <a href="tel:+923240539139">+92 324-0539-139</a>
+                <div className="Email">
+                    <div className="empty"></div>
+                    <div className="contact">
+                        <p>Email</p>
+                    </div>
+                </div>
+                <a href="mailto:nofil.khan037@gmail.com">Nofil.khan037@gmail.com</a>
+
+                <div className="Github">
+                    <div className="empty"></div>
+                    <div className="contact">
+                        <p>Accounts</p>
+                    </div>
+                </div>
+                <a href="https://accountlinks-150e1.web.app/">Accounts Link </a>
                 
+                <div className="address">
+                    <div className="empty"></div>
+                    <div className="contact">
+                        <p>Address</p>
+                    </div>
+                </div>
+
             </div>
             <div className="Black">
             </div>
